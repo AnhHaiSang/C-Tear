83e0be515d3990343c28d923f2fe2568092cf9f4
mrnhats <minhnhat15c2@gmail.com>
2019-12-12 00:59:51 +0700

    sangpatch12122019

--- src/Actions/user/ActionUser.js
+++ src/Actions/user/ActionUser.js
@@ -1,17 +1,25 @@
 import * as Types from '../../Constants/user/ActionTypes';
 
-export const AddToCart = (product, total) =>{
-    return{
+export const actAddToCart = (product) => {
+    return {
         type: Types.ADD_TO_CART,
         product,
-        total
-    }
-} 
 
-export const DauCong = (product, total) => {
-    return{
+    }
+}
+
+export const DeleteProductInCart = (product) => {
+    return {
+        type: Types.DELETE_PRODUCT_IN_CART,
+        product
+
+    }
+}
+export const actUpdateProductInCart = (product, quantaity) => {
+    return {
         type: Types.UPDATE_PRODUCT_IN_CART,
         product,
-        total
+        quantaity
+
     }
 }
--- src/Components/user/lists_product.js
+++ src/Components/user/lists_product.js
@@ -5,9 +5,8 @@ import { Link } from 'react-router-dom';
 
 class ListsProduct extends React.Component {
     render() {
-        var { products } = this.props;
-        // console.log(this.props);
-        let total;
+        var { products} = this.props;
+        // console.log(products);
         return (
             <div className="items1">
                 <div className="card_items">
@@ -25,23 +24,22 @@ class ListsProduct extends React.Component {
                                 </ul>
                             </h4>
                             <p className="price">${products.price}</p>
-                            </div>
-                            <div className="button_items">
-                                <button onClick={()=>this.addcart(products,total)} type="button" className="btn btn-primary">Add to Cart</button>
-                            </div>
-                        
+                        </div>
+                        <div className="button_items">
+                            <button onClick={() => this.onAddToCart(products)} type="button" className="btn btn-primary">Add to Cart</button>
+                        </div>
+
                         <div className="clr"></div>
                     </Link>
                 </div>
             </div>
-
-        )
+ 
+        ) 
+    }  
+    onAddToCart = (product) => {   
+        this.props.onAddToCart(product);
     }
-    addcart = (product,total) => {
-        this.props.addtocart(product,total);
-        // console.log(this.props.addtocart(product,total));
-        
-    }
+    
     showReview(review) {
         var result = [];
         for (var i = 1; i <= review; i++) {
